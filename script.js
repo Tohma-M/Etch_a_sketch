a7b19fea5b901ec0bc62f7796b13c6e90e8d83b2
Tohma-M <mitsuyat5220@gmail.com>
2024-06-24 20:27:25 -0400

    kind of done. Getting lazy though

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function makeGrid(dimension) {
     container.replaceChildren();
     container.style.background = "white";
     container.style.borderStyle = "solid";
-    for (i = 0; i <= dimension; i++) {
+    for (i = 0; i < dimension; i++) {
         var newRow = document.createElement("div"); 
         newRow.id = "row" + i; 
         newRow.setAttribute ("style", "display: flex; flex: 1"); 
         container.appendChild(newRow); 
-        for (j = 0; j <= dimension; j ++) {
+        for (j = 0; j < dimension; j ++) {
             var newDiv = document.createElement ("div"); 
             newDiv.id = i + "," + j; 
             newDiv.setAttribute("style", "flex: 1; flex; border-style: solid; border-color: black; opacity: 0"); 
@@ -29,4 +29,28 @@ function makeGrid(dimension) {
     }
 }
 
+// activates when mouse is over a div
+container.addEventListener("mouseover", onMouseOver);
 makeGrid(dimension); 
+
+
+/*Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
+There are multiple ways to change the color of the divs, including:
+Adding a new class to the div.
+Changing the div’s background color using JavaScript.
+*/
+function onMouseOver (event) {
+    let target = event.target;
+    if (!target.classList.contains("container")) {
+        // Logic for elements that are not the container itself
+        target.style.background = "blue";
+        let opacity = parseFloat(target.style.opacity);
+        if (opacity < 1) {
+            target.style.opacity = opacity + 0.1;
+        }
+    }
+}
+
+
+
